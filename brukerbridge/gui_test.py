487bb800d11d322840ad6f1e3e64c39e7ac8517e
Luke Brezovec <brezovec@stanford.edu>
2020-09-11 13:54:21 -0700

    cleaning up files

--- brukerbridge/gui_test.py
+++ brukerbridge/gui_test.py
@@ -1,6 +1,6 @@
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askdirectory
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename(initialdir = "G:/ftp_imports") # show an "Open" dialog box and return the path to the selected file
+filename = askdirectory(initialdir = "G:/ftp_imports") # show an "Open" dialog box and return the path to the selected file
 print(filename)
