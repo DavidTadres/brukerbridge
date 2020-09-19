8b6841dddf1ae47d412005c3470f6b77ab1dfb1f
Luke Brezovec <brezovec@stanford.edu>
2020-09-19 14:29:53 -0700

    sync

--- brukerbridge/server.py
+++ brukerbridge/server.py
@@ -64,8 +64,8 @@ while True:
     print("USER: {}".format(user))
     print("DIRECTORY: {}".format(directory))
     sys.stdout.flush()
-    os.system("python C:/Users/User/projects/brukerbridge/scripts/main.py '{}' '{}'".format(user, directory))
-    # added single quotes to accomidate spaces in directory name
+    os.system('python C:/Users/User/projects/brukerbridge/scripts/main.py "{}" "{}"'.format(user, directory))
+    # added double quotes to accomidate spaces in directory name
 
     # email user informing of success or failure, and send relevant log file info
     os.system("python C:/Users/User/projects/brukerbridge/scripts/final_email.py")
