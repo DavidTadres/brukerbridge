d8a30ab5ce5fdcb09320ba80ed28d41f10d7831c
lukebrez <brezovec@stanford.edu>
2020-09-19 14:03:45 -0700

    sync

--- scripts/run_partial.py
+++ scripts/run_partial.py
@@ -3,19 +3,19 @@ import brukerbridge as bridge
 extensions_for_oak_transfer = ['.nii', '.csv', '.xml', 'json'] # last 4 chars
 
 ### Directory on this computer to process ###
-full_target = 'F:/ftp_imports/20200130'
+full_target = 'G:/Ashley/20200913__flag__'
 
 ### Oak target ###
 # oak_target = 'X:/data/Brezovec/2P_Imaging/imports'
-#oak_target = 'X:/data/Ashley2/imports'
-oak_target = 'X:/data/Max/ImagingData/Bruker/imports'
+oak_target = 'X:/data/Ashley2/imports'
+#oak_target = 'X:/data/Max/ImagingData/Bruker/imports'
 
 ### raw to tiff ###
-bridge.convert_raw_to_tiff(full_target)
+#bridge.convert_raw_to_tiff(full_target)
 
 ### tiffs to nii or tiff stack ###
-bridge.convert_tiff_collections_to_nii(full_target)
+#bridge.convert_tiff_collections_to_nii(full_target)
 #bridge.convert_tiff_collections_to_stack(full_target)
 
 ### Transfer to oak ###
-bridge.start_oak_transfer(full_target, oak_target, extensions_for_oak_transfer)
+bridge.start_oak_transfer(full_target, oak_target, extensions_for_oak_transfer, "False")
