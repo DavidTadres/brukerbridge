319a1088f03575b313777858bf94b31de92fa7cb
lukebrez <brezovec@stanford.edu>
2021-08-06 10:44:58 -0700

    update 8-6-21

--- scripts/run_partial.py
+++ scripts/run_partial.py
@@ -3,15 +3,18 @@ import brukerbridge as bridge
 extensions_for_oak_transfer = ['.nii', '.csv', '.xml', 'json'] # last 4 chars
 
 ### Directory on this computer to process ###
-full_target = 'G:/Ashley/20210702'
+#full_target = 'G:/Ashley/20210702'
+# full_target = 'F:/Max/20210804'
+full_target = 'F:/Michelle/210805'
 
 
 ### Oak target ###
 # oak_target = 'X:/data/Brezovec/2P_Imaging/imports'
-oak_target = 'X:/data/Ashley2/imports'
-#oak_target = 'X:/data/Max/ImagingData/Bruker/imports'
+#oak_target = 'X:/data/Ashley2/imports'
+# oak_target = 'X:/data/Max/ImagingData/Bruker/imports'
 #oak_target = 'X/data/Ina/bruker_data/imports'
 #oak_target = X:/data/Avery/bruker_data/imports'
+oak_target = 'X:/data/Michelle/Bruker/imports'
 
 ### raw to tiff ###
 bridge.convert_raw_to_tiff(full_target)
