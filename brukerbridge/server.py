251a53c65347a8cf161ed73d1d04f84ec7d48e24
Luke Brezovec <brezovec@stanford.edu>
2022-03-05 18:14:11 -0800

    added checksum

--- brukerbridge/server.py
+++ brukerbridge/server.py
@@ -34,7 +34,7 @@ while True:
                 print('ALL_FILES_TRANSFERED')
                 all_checksums_true = False not in all_checksums_match
                 message = str(len(all_checksums_true)) + "." + str(succeful_transfer)
-                client.sendall(message.encode())
+                #client.sendall(message.encode())
                 client.sendall("BOO".encode())
                 break
             if not raw: break # no more files, server closed connection.
