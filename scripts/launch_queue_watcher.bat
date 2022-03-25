8fd6fa834dba14b7d64e9f705e39d204b6763bbe
Luke Brezovec <brezovec@stanford.edu>
2022-03-25 13:51:46 -0700

    printlog

---
+++ scripts/launch_queue_watcher.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\WINDOWS\system32\cmd.exe python projects\brukerbridge\scripts\queue_watcher.py"
+@echo "HELLO I AM THE QUEUE WATCHER."
+@echo "IF I FIND A QUEUED DIRECTORY I WILL LAUNCH MAIN PROCESSING."
+cmd /k
---
+++ scripts/queue_watcher.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import time
+
+log_folder = 'C:/Users/User/Desktop/dataflow_logs'
+root_directory = "H:/"
+
+def main():
+
+	while True:
+		queued_folder = get_queued_folder()
+		if queued_folder is not None:
+			launch_main_processing(queued_folder, log_folder) 
+		time.sleep(0.1)
+
+def get_queued_folders():
+	low_queue = None
+    for user_folder in os.listdir(root_directory):
+    	user_folder = os.path.join(root_directory, user_folder)
+
+    	if os.path.isdir(user_folder):
+	    	for potential_queued_folder in os.listdir(user_folder):
+	    		potential_queued_folder = os.path.join(user_folder, potential_queued_folder)
+
+	    		if potential_queued_folder.endswith('__queue__'):
+	    			return potential_queued_folder ### Immediately return any queued folder found
+
+	    		if potential_queued_folder.endswith('__lowqueue__'):
+	    			low_queue = potential_queued_folder
+
+	return low_queue
+
+def launch_main_processing(dir_to_process, log_folder)
+    log_file = 'dataflow_log_' + strftime("%Y%m%d-%H%M%S") + '.txt'
+    full_log_file = os.path.join(log_folder, log_file)
+
+    # the >> redirects stdout with appending. the 2>&1 does the same with stderr
+    # double quotes to accommodates spaces in directory name
+    # this command is blocking so this watcher will just wait here until main.py is finished
+	os.system('python C:/Users/User/projects/brukerbridge/scripts/main.py "{}" >> {} 2>&1'.format(dir_to_process, full_log_file))
+
+if __name__ == '__main__':
+    main()
