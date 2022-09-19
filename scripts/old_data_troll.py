8c72561f7d793375dd2ca20c417c9433ee560037
Luke Brezovec <brezovec@stanford.edu>
2022-09-19 13:19:49 -0700

    fixed email

--- scripts/old_data_troll.py
+++ scripts/old_data_troll.py
@@ -13,19 +13,23 @@ def main():
 
 	email = "brezovec@stanford.edu"
 	error_info = "hello"
-	bridge.send_email(subject='BrukerBridge FAILED', message=error_info, recipient=email)
+	#bridge.send_email(subject='BrukerBridge FAILED', message=error_info, recipient=email)
 
-	# while True:
-	# 	for user_folder in os.listdir(root_directory):
-	# 		### need to skip this weird file
-	# 		if user_folder == 'System Volume Information':
-	# 			continue
-	# 		user_folder = os.path.join(root_directory, user_folder)
+	while True:
+		for user_folder in os.listdir(root_directory):
+			### need to skip this weird file
+			if user_folder == 'System Volume Information':
+				continue
+			user_folder = os.path.join(root_directory, user_folder)
 
-	# 		if os.path.isdir(user_folder):
-	# 			for potential_old_folder in os.listdir(user_folder):
+			if os.path.isdir(user_folder):
+				for potential_old_folder in os.listdir(user_folder):
+					creation_time = os.path.getctime(potential_old_folder)
+					age_in_seconds = creation_time - time.time()
+					age_in_days = age_in_seconds/(60*60*24)
+					print(F"{potential_old_folder}: {age_in_days}")
 
-	# 	time.sleep(0.1)
+		time.sleep(0.1)
 
 if __name__ == '__main__':
 	main()
