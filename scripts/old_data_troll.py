eddfde2cf017298be161fe25cd4814e5be146f61
Luke Brezovec <brezovec@stanford.edu>
2022-09-19 13:58:51 -0700

    fixed email

--- scripts/old_data_troll.py
+++ scripts/old_data_troll.py
@@ -38,9 +38,9 @@ def main():
 			print("hi1")
 			for potential_old_folder in os.listdir(user_folder):
 				print("hi2")
+				potential_old_folder = os.path.join(user_folder, potential_old_folder)
 				if os.path.isdir(potential_old_folder):
 					print("hi3")
-					potential_old_folder = os.path.join(user_folder, potential_old_folder)
 					creation_time = os.path.getctime(potential_old_folder)
 					age_in_seconds = time.time() - creation_time
 					age_in_days = age_in_seconds/(60*60*24)
