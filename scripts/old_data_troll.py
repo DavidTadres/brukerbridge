b35b539c9d1fc36810605a5d8927a371f0227701
Luke Brezovec <brezovec@stanford.edu>
2022-09-19 13:55:43 -0700

    fixed email

--- scripts/old_data_troll.py
+++ scripts/old_data_troll.py
@@ -15,55 +15,55 @@ root_directory = "H:/"
 def main():
 
 	print("HELLO I AM THE OLD DATA TROLL")
-	
+
 	age_limit = 60 # in days
 	exception_folders = ['fictrac', '$RECYCLE.BIN']
 	exception_flag = "_old_"
 
-	while True:
-		users_with_old_files = []
-		old_files = []
-		for user_folder in os.listdir(root_directory):
-			### need to skip this weird file
-			if user_folder == 'System Volume Information':
-				continue
-			if user_folder in exception_folders:
-				continue
+	#while True:
+	users_with_old_files = []
+	old_files = []
+	for user_folder in os.listdir(root_directory):
+		### need to skip this weird file
+		if user_folder == 'System Volume Information':
+			continue
+		if user_folder in exception_folders:
+			continue
 
-			user = user_folder
-			user_folder = os.path.join(root_directory, user_folder)
+		user = user_folder
+		user_folder = os.path.join(root_directory, user_folder)
 
-			if os.path.isdir(user_folder):
-				for potential_old_folder in os.listdir(user_folder):
-					if os.path.isdir(potential_old_folder):
-						potential_old_folder = os.path.join(user_folder, potential_old_folder)
-						creation_time = os.path.getctime(potential_old_folder)
-						age_in_seconds = time.time() - creation_time
-						age_in_days = age_in_seconds/(60*60*24)
-						if age_in_days > age_limit and exception_flag not in potential_old_folder:
-							print(F"{potential_old_folder}: {age_in_days}")
-							users_with_old_files.append(user)
-							old_files.append(potential_old_folder)
+		if os.path.isdir(user_folder):
+			for potential_old_folder in os.listdir(user_folder):
+				if os.path.isdir(potential_old_folder):
+					potential_old_folder = os.path.join(user_folder, potential_old_folder)
+					creation_time = os.path.getctime(potential_old_folder)
+					age_in_seconds = time.time() - creation_time
+					age_in_days = age_in_seconds/(60*60*24)
+					if age_in_days > age_limit and exception_flag not in potential_old_folder:
+						print(F"{potential_old_folder}: {age_in_days}")
+						users_with_old_files.append(user)
+						old_files.append(potential_old_folder)
 
-		for user in np.unique(users_with_old_files):
-			json_file = os.path.join(users_directory, user + '.json')
-			try:
-				with open(json_file) as file:
-					settings = json.load(file)
-					email = settings.get('email')
-			except:
-				print(F"{user} has no json file to get email from. they should create one in {users_directory}")
-				continue
+	for user in np.unique(users_with_old_files):
+		json_file = os.path.join(users_directory, user + '.json')
+		try:
+			with open(json_file) as file:
+				settings = json.load(file)
+				email = settings.get('email')
+		except:
+			print(F"{user} has no json file to get email from. they should create one in {users_directory}")
+			continue
 
-			users_old_files = np.asarray(old_files)[np.where(user==np.asarray(users_with_old_files))[0]]
-			print(users_old_files)
+		users_old_files = np.asarray(old_files)[np.where(user==np.asarray(users_with_old_files))[0]]
+		print(users_old_files)
 
-			message = F"You have the following old directories (older than {age_limit} days):\n {users_old_files}"
-			if user == 'luke':
-				bridge.send_email(subject='BrukerBridge Old Data Troll Says Hello!', message=message, recipient=email)
+		message = F"You have the following old directories (older than {age_limit} days):\n {users_old_files}"
+		if user == 'luke':
+			bridge.send_email(subject='BrukerBridge Old Data Troll Says Hello!', message=message, recipient=email)
 
 
-		time.sleep(100)
+		#time.sleep(100)
 
 if __name__ == '__main__':
 	main()
