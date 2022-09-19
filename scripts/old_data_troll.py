da01205ec0c977e015e45a02d65aeb729c7ffdcf
Luke Brezovec <brezovec@stanford.edu>
2022-09-19 13:33:30 -0700

    fixed email

--- scripts/old_data_troll.py
+++ scripts/old_data_troll.py
@@ -20,6 +20,7 @@ def main():
 	#bridge.send_email(subject='BrukerBridge FAILED', message=error_info, recipient=email)
 
 	#while True:
+	users_with_old_files = []
 	for user_folder in os.listdir(root_directory):
 		### need to skip this weird file
 		if user_folder == 'System Volume Information':
@@ -27,16 +28,20 @@ def main():
 		if user_folder in exception_folders:
 			continue
 
+		user = user_folder
 		user_folder = os.path.join(root_directory, user_folder)
 
 		if os.path.isdir(user_folder):
 			for potential_old_folder in os.listdir(user_folder):
-				potential_old_folder = os.path.join(user_folder, potential_old_folder)
-				creation_time = os.path.getctime(potential_old_folder)
-				age_in_seconds = time.time() - creation_time
-				age_in_days = age_in_seconds/(60*60*24)
-				if age_in_days > age_limit and exception_flag not in potential_old_folder:
-					print(F"{potential_old_folder}: {age_in_days}")
+				if os.path.isdir(user_folder):
+					potential_old_folder = os.path.join(user_folder, potential_old_folder)
+					creation_time = os.path.getctime(potential_old_folder)
+					age_in_seconds = time.time() - creation_time
+					age_in_days = age_in_seconds/(60*60*24)
+					if age_in_days > age_limit and exception_flag not in potential_old_folder:
+						print(F"{potential_old_folder}: {age_in_days}")
+						users_with_old_files.append(user)
+	print(np.unique(users_with_old_files))
 
 	#time.sleep(0.1)
 
