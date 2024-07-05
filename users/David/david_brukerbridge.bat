f0775395b02fa7eb901e4c84c126bfa756085223
David Tadres <dtadres@stanford.edu>
2024-07-04 17:21:58 -0700

    fix python path on bruker computer

--- users/David/david_brukerbridge.bat
+++ users/David/david_brukerbridge.bat
@@ -2,5 +2,5 @@
 @echo C:\Users\User\AppData\Local\Programs\Python\Python37\python.exe
 SET mypath=%~dp0
 @echo calling %mypath:~0,-13%\brukerbridge\Imaging_PC\david_client.py
-C:\Users\David\.conda\envs\env_brukerbridge\python.exe %mypath:~0,-13%\brukerbridge\Imaging_PC\david_client.py
+C:\Users\User\AppData\Local\Programs\Python\Python37\python.exe %mypath:~0,-13%\brukerbridge\Imaging_PC\david_client.py
 cmd /k
