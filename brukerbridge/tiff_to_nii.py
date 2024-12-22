dc02321136dd37cfd72b9ff3d28121b8979c3aeb
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-12-22 01:13:21 -0800

    update to also look for hdf5 files in individual fly folders

--- brukerbridge/tiff_to_nii.py
+++ brukerbridge/tiff_to_nii.py
@@ -296,7 +296,19 @@ def convert_tiff_collections_to_nii(directory,
         # First, create fly.json file for each folder based on the h5 file
         # created by stimpack
         try:
+            print('first trying to look for one hdf5 in: ' + str(directory))
             utils.get_fly_json_data_from_h5(directory)
+            # If able to create all fly.json, set this to True
+            fly_json_already_created = True
+            print('Successfully created fly.json files from from stimpack h5 file')
+        except Exception as e:
+            print('******* WARNING ******')
+            print('unable to create fly.json files from h5 because:')
+            print(e)
+
+        try:
+            print('instead trying to look for one hdf5 in each fly folder in : ' + str(directory))
+            utils.get_fly_json_data_from_h5_one_fly_per_hdf5(directory)
             # If able to create all fly.json, set this to True
             fly_json_already_created = True
             print('Successfully created fly.json files from from stimpack h5 file')
