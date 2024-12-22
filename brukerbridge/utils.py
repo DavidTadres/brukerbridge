613297f997dd79b3c6fe43668723a8f41aa87446
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-12-22 02:05:37 -0800

    Update utils.py

--- brukerbridge/utils.py
+++ brukerbridge/utils.py
@@ -328,7 +328,7 @@ def get_fly_json_data_from_h5_one_fly_per_hdf5(directory):
 
             fly_dict = {}
             print('genotype father: ' + repr(subject['genotype_father']))
-            for current_attrs in subject[current_subject].attrs:
+            for current_attrs in subject[str(current_subject)].attrs:
                 fly_dict[current_attrs] = subject[current_subject].attrs[current_attrs]
 
             # Save the dict, can use it to directly write the fly.json file as well!
