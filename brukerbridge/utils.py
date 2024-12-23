d7a8a65ef1e17d6cc1293ecd8f32e01427a67967
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-12-22 19:41:12 -0800

    Update utils.py

--- brukerbridge/utils.py
+++ brukerbridge/utils.py
@@ -327,7 +327,7 @@ def get_fly_json_data_from_h5_one_fly_per_hdf5(directory):
             # Look for a folder that should be the subjects folder!
 
             fly_dict = {}
-            print('genotype father: ' + repr(subject['genotype_father']))
+            print('genotype father: ' + repr(subject[str(current_subject)].attrs['genotype_father']))
             for current_attrs in subject[str(current_subject)].attrs:
                 fly_dict[current_attrs] = subject[str(current_subject)].attrs[current_attrs]
 
