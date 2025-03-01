8123518005ccea798a67f67196e46886c755658d
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-03-01 09:35:40 -0800

    Revert "update server to save data to #send data folder to "C:/Users/jcsimon/Documents/Stanford/Data/Bruker/imports/" instead of #send data folder to "C:/Users/jcsimon/Documents/Stanford/Data/Bruker/imports/Jacob""
    This reverts commit 1c405c8cd771c0f056451d1710b8c576901c3d97.

--- brukerbridge/ripping_PC/jacob_server.py
+++ brukerbridge/ripping_PC/jacob_server.py
@@ -146,12 +146,11 @@ while True:
 	# pathlib.Path(path).relative_to(target_directory)
 	# would yield i.e. WindowsPath('David/20240404__queue__/fly3/anat0/TSeries-12172018-1322-002')
 	# This will yield i.e. 'David'
-	user_folder = pathlib.Path(path).relative_to(target_directory).parts[0] # pretty sure it extracts user folder name folder from imaging pc (gives Jacob instead of jcsimon)
+	user_folder = pathlib.Path(path).relative_to(target_directory).parts[0]
 	# This will yield i.e. '20240606'
 	dir_to_flag = pathlib.Path(path).relative_to(target_directory).parts[1]
 
-	#full_path = pathlib.Path(target_directory, user_folder, dir_to_flag) #send data folder to "C:/Users/jcsimon/Documents/Stanford/Data/Bruker/imports/Jacob"
-	full_path = pathlib.Path(target_directory, dir_to_flag) #send data folder to "C:/Users/jcsimon/Documents/Stanford/Data/Bruker/imports/"
+	full_path = pathlib.Path(target_directory, user_folder, dir_to_flag)
 
 	#print(dir_to_flag, flush=True)
 	#os.rename(dir_to_flag, dir_to_flag + '__queue__')
