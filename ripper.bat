8a3a7e2c3785054f7191dfb510f277eff2c4fdc8
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-05-19 12:06:46 -0700

    update ripper.bat to delete raw files again (causes annoying problems with ripper killer)

--- ripper.bat
+++ ripper.bat
@@ -5,5 +5,5 @@ set PVSCAN_VERSION=%2
 echo Ripping from folder %FOLDER_NAME%.
 echo Using PVScan version %PVSCAN_VERSION%
 SET mypath=%~dp0
-%mypath:~0,-1%"\Prairie %PVSCAN_VERSION%\Utilities\Image-Block Ripping Utility.exe" "-IncludeSubFolders" "-KeepRaw" "-AddRawFileWithSubFolders" %FOLDER_NAME% "-Convert"
+%mypath:~0,-1%"\Prairie %PVSCAN_VERSION%\Utilities\Image-Block Ripping Utility.exe" "-IncludeSubFolders" "-AddRawFileWithSubFolders" %FOLDER_NAME% "-Convert"
 echo Finished ripping: %date% %time%
