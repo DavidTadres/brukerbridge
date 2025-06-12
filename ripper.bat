c35c44711503a86f6315364a26c3a3c8d3f8497c
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-06-12 13:14:24 -0700

    Update ripper.bat

--- ripper.bat
+++ ripper.bat
@@ -3,7 +3,7 @@ echo Started ripper: %date% %time%
 set FOLDER_NAME=%1
 set PVSCAN_VERSION=%2
 echo Ripping from folder %FOLDER_NAME%.
-echo Using PVScan version %PVSCAN_VERSION%
-SET mypath=%~dp0
-%mypath:~0,-1%"\Prairie %PVSCAN_VERSION%\Utilities\Image-Block Ripping Utility.exe" "-IncludeSubFolders" "-AddRawFileWithSubFolders" %FOLDER_NAME% "-Convert"
+echo Using PVScan version %PVSCAN_VERSION%.
+::"C:\Program Files\Prairie %PVSCAN_VERSION%\Prairie View\Utilities\Image-Block Ripping Utility.exe"  "-IncludeSubFolders" "-AddRawFileWithSubFolders" %FOLDER_NAME% "-Convert"
+"C:\Program Files\Prairie %PVSCAN_VERSION%\Prairie View\Utilities\Image-Block Ripping Utility.exe"  "-IncludeSubFolders" "-AddRawFileWithSubFolders" %FOLDER_NAME% "-Convert"
 echo Finished ripping: %date% %time%
