5e40892e8931873c8a0070f04e38ed5baccfe60f
David Tadres <dtadres@stanford.edu>
2026-04-02 13:59:58 -0700

    Here's the summary of what's staged:
      - LFS: 158 Prairie binary files migrated from regular git to LFS tracking (via .gitattributes)
      - Deleted: not_used/ (8 files), david_client_dev.py, server_dev.py, dev_test.py, rippper_test.bat, ripper_dev.bat
      - Untracked: .DS_Store files removed from index
      - .gitignore: Added .DS_Store and env_*

--- brukerbridge/not_used/gui_test.py
+++
@@ -1,6 +0,0 @@
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askdirectory
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askdirectory(initialdir = "G:/ftp_imports") # show an "Open" dialog box and return the path to the selected file
-print(filename)
